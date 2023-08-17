3afabdde4cb722c5642337c57ac5495e4df6ac3c
Wietsevdv <Wietsevdv@users.noreply.github.com>
2023-08-17 18:43:04 +0200

    Initial commit

---
+++ GOAP.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class GOAP final
+{
+public:
+	GOAP(GOAP&& other) = delete;
+	GOAP(const GOAP& other) = delete;
+	GOAP operator=(GOAP&& other) = delete;
+	GOAP operator=(const GOAP&& other) = delete;
+
+private:
+
+};
+
